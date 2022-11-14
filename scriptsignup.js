bac2908e0ba0c65b15b5aa9b9275c7ee15b451c2
Archawin <100111704+iceisice4@users.noreply.github.com>
2022-11-14 16:14:37 +0700

    Add sign in
    Add sign up

---
+++ scriptsignup.js
@@ -0,0 +1,6 @@
+let openLoginRight = document.querySelector('.h1');
+let loginWrapper = document.querySelector('.login-warpper');
+
+openLoginRight.addEventListener('click' , function(){
+    loginWrapper.classList.toggle('open');
+});
