2c6ec6e37c483c39b30382427e2c5d00413937db
Archawin <100111704+iceisice4@users.noreply.github.com>
2022-11-17 13:26:08 +0700

    Cart!!!!
    เหลือเอาไปใช้กับWebเรา

--- php/cart2.php
+++ php/cart2.php
@@ -1,9 +1,57 @@
 <?php
     session_start();
     require_once('dbcontroller.php');
-    $db_handle = new DBController()
+    $db_handle = new DBController();
 
+    if(!empty($_GET["action"])) {
+        switch($_GET["action"]) {
+            case "add";
+                if(!empty($_POST["quantity"])) {
+                    $productByCode = $db_handle->runQuery("SELECT * FROM tblproduct WHERE code='".$_GET["code"]."'");
+                    $itemArray = array($productByCode[0] ["code"] =>(array('name' => $productByCode[0]["name"], 
+                                                                            'code'=> $productByCode[0]["code"],
+                                                                            'quantity' => $_POST["quantity"],
+                                                                            'price'=> $productByCode[0]["price"],
+                                                                            'image' => $productByCode[0]["image"])));
+                }
 
+                if(!empty($_SESSION["cart_item"])){
+                    if(in_array($productByCode[0]["code"],array_keys($_SESSION["cart_item"]))) {
+
+                        foreach($_SESSION["cart_item"] as $k => $v){
+                            if($productByCode[0]["code"] == $k){
+                                if(empty($_SESSION["cart_item"][$k]["quantity"])){
+                                    $_SESSION["cart_item"][$k]["quantity"] = 0;
+                                }
+                                $_SESSION["cart"][$k]["quantity"] += $_POST["quantity"];
+                            }
+                        }
+                    } else {
+
+                        $_SESSION['cart_item'] = array_merge($_SESSION['cart_item'],$itemArray);
+                    }
+                } else {
+                    $_SESSION["cart_item"] = $itemArray;
+            }
+            break;
+            case "remove":
+                if(!empty($_SESSION["cart_item"])){
+                    foreach($_SESSION["cart_item"] as $k => $v ){
+                        if($_GET["code"] == $k)
+                            unset($_SESSION["cart_item"][$k]);
+
+                        if(empty($_SESSION["cart_item"]))
+                            unset($_SESSION["cart_item"]);
+                    }
+                }
+            break;
+            case "empty":
+                unset($_SESSION["cart_item"]);
+            break;
+        }
+        
+    }
+    
 ?>
 
 
@@ -14,14 +62,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Shopping Cart</title>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" />
-    <link rel="stylesheet" href="/css/style_cart.css">
+    
+    <link rel="stylesheet" href="/Preview_JectWeb/css/style_cart.css">
 </head>
 <body>
 
     <div id="shopping-cart">
         <div class="txt-heading">Shopping Cart</div>
-        <a href="index.php?action=empty" class="btnEmpty">Empty Cart</a>
+        <a href="cart2.php?action=empty" class="btnEmpty">Empty Cart</a>
+
+        <?php
+
+        if(isset($_SESSION["cart_item"])){
+            $total_quantity = 0;
+            $total_price = 0;
+        
+
+
+        ?>
+
 
         <table class="tbl-cart" cellpadding="10" cellspacing="1">
             <tbody>
@@ -33,24 +92,50 @@
                     <th style="text-align: right;" width="10%">Price</th>
                     <th style="text-align: center;" width="5%">Remove</th>
                 </tr>
+
+                <?php
+                    foreach($_SESSION["cart_item"] as $item){
+                        $item_price = $item["quantity"] * $item["price"]
+                    
+
+                ?>
+
                 <tr>
-                    <td><img src="/img/outdent.png" width="150" class="cart-item-image" alt=""></td>
-                    <td>Dref21a</td>
-                    <td style="text-align: right">1</td>
-                    <td style="text-align: right">1000 Bath</td>
-                    <td style="text-align: right;">1000 Bath</td>
-                    <td style="text-align: center;"><a href="#" class="btnRemoveAction"><img src="/Preview_JectWeb/img/close.png" width="7" alt="Remove Item"></a></td>
+                    <td><img src="<?php echo $item["image"];?>" width="150" class="cart-item-image" alt="">
+                    <?php echo $item["name"]; ?>
+                    </td>
+                    <td><?php echo $item["code"]; ?></td>
+                    <td style="text-align: right"><?php echo $item["quantity"]; ?></td>
+                    <td style="text-align: right"><?php echo "$ ". $item["price"]; ?></td>
+                    <td  style="text-align:right;"><?php echo "$ ". number_format($item_price,2); ?></td>
+                    <td style="text-align:center;"><a href="cart2.php?action=remove&code=<?php echo $item["code"]; ?>" class="btnRemoveAction"><img src="/Preview_JectWeb/img/close.png" width="10" alt="Remove Item" /></a></td>
                 </tr>
+                
+                <?php
+                    $total_quantity += $item["quantity"];
+                    $total_price += ($item["price"] * $item["quantity"]);
+
+                    }
+                ?>
+
                 <tr>
                     <td colspan="2" align="right">Total:</td>
-                    <td align="right">1</td>
-                    <td align="right" colspan="2">1000 Bath</td>
+                    <td align="right"><?php echo $total_quantity; ?></td>
+                    <td align="right" colspan="2"><?php  echo "$ " . number_format($total_price,2); ?></td>
                     <td></td>
                 </tr>
             </tbody>
             
 
         </table>
+        <?php
+            } else {
+
+        ?>
+        <div class="no-records">Your Cart Is Empty</div>
+        <?php
+            }
+        ?>
     </div>
     <div id="product-grid">
         <div class="txt-heading">Products</div>
@@ -69,7 +154,7 @@
         
 
         <div class="product-item">
-            <form action="index.php?action=add&code">
+            <form action="cart2.php?action=add&code=<?php echo $product_array[$key]["code"];?>" method="post">
                 <div class="product-image">
                     <img src="<?php echo $product_array[$key]["image"]; ?>"  width="200" alt="images">
                 </div>
